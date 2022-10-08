f6fd2c8af17e6fa7a2d5ece7c1870f9a7657ea3e
zhuaq <zhuaq@7000mall.com>
2022-10-08 10:44:49 +0800

    fixed bug fix修复根目录错误

--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@ const path = require("node:path");
 const app = new Koa();
 app.use(parser()).use(cors());
 app.use(router.routes());
-app.use(async (ctx) => {
-  ctx.body =
-    '<div style="width: 100%;height: 100%;display: flex;align-items: center;justify-content: center;">文档地址：<a href="https://zhuaiquan.github.io/haokan.video/#/">传送门</a></div>';
-});
 app.use(static(path.join(__dirname, "../public")));
 
 app.on("error", (err, ctx) => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,22 @@
-const Router = require('koa-router');
-const recomment = require('./recomment');
-const detail = require('./detail');
-const like = require('./like');
-const search = require('./search');
-const top = require('./top');
-const tv = require('./tv');
+const Router = require("koa-router");
+const recomment = require("./recomment");
+const detail = require("./detail");
+const like = require("./like");
+const search = require("./search");
+const top = require("./top");
+const tv = require("./tv");
 const router = new Router();
 
-router.use('/recomment', recomment.routes(), recomment.allowedMethods());
-router.use('/detail', detail.routes(), detail.allowedMethods());
-router.use('/like', like.routes(), like.allowedMethods());
-router.use('/search', search.routes(), search.allowedMethods());
-router.use('/top', top.routes(), top.allowedMethods());
-router.use('/tv', tv.routes(), tv.allowedMethods());
+router.use("/recomment", recomment.routes(), recomment.allowedMethods());
+router.use("/detail", detail.routes(), detail.allowedMethods());
+router.use("/like", like.routes(), like.allowedMethods());
+router.use("/search", search.routes(), search.allowedMethods());
+router.use("/top", top.routes(), top.allowedMethods());
+router.use("/tv", tv.routes(), tv.allowedMethods());
+// 首页
+router.get("/", async (ctx) => {
+  ctx.body =
+    '<div style="width: 100%;height: 100%;display: flex;align-items: center;justify-content: center;">文档地址：<a href="https://zhuaiquan.github.io/haokan.video/#/">传送门</a></div>';
+});
 
 module.exports = router;
